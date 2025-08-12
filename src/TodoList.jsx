b5c5873540e8420aab13a5ba1329eb69a5c20573
ankushpille <ankush.pille1@gmail.com>
2025-08-12 21:00:10 +0530

    building todo app

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,16 +1,29 @@
 import './App.css'
+import { useState } from 'react'
 
 export default function TodoList(){
+  const [text,setText] = useState('')
 
+   const handlechange = (e) => {
+    setText(e.target.value)
+   }
+
+   const handleOnclick = (e) => {
+    setText(e.target.value)
+   }
 
     return (
 
         <>
         <div className='container'> 
         <h1>TodoList</h1>
-        <input type="text" />
-        <button id="addButton">Add</button>
+        <input type="text" onChange={handlechange}/>
+        <button id="addButton" onClick={handleOnclick}>Add</button>
+         <h1>{text}</h1>
+          
         </div>
+
+        
         </>
     )
 }
