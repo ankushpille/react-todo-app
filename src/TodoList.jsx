dcdbef42a6d875de340c6ab96fe316f8fe59ed03
ankushpille <ankush.pille1@gmail.com>
2025-08-14 20:45:55 +0530

    todolist example

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -3,14 +3,17 @@ import { useState } from 'react'
 
 export default function TodoList(){
   const [text,setText] = useState('')
+  const [inputValue,setInputvalue] = useState([])
 
    const handlechange = (e) => {
     setText(e.target.value)
    }
 
-   const handleOnclick = (e) => {
-    setText(e.target.value)
+   const handleOnclick = () => {
+     setInputvalue([...inputValue,text])
    }
+
+   console.log("inputvalue",inputValue)
 
     return (
 
@@ -19,7 +22,11 @@ export default function TodoList(){
         <h1>TodoList</h1>
         <input type="text" onChange={handlechange}/>
         <button id="addButton" onClick={handleOnclick}>Add</button>
-         <h1>{text}</h1>
+         <ul>
+            {inputValue.map((text,index) => (
+                 <li key={index}>{text}</li>
+            ))}
+         </ul>
           
         </div>
 
